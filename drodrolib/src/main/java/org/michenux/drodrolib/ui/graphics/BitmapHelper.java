c51dba070cce6c886cacb9b48f3fdcccd5a5e85f
Frieder Bluemle <frieder.bluemle@gmail.com>
2014-08-05 23:51:22 -0700

    Fix Lint errors

--- drodrolib/src/main/java/org/michenux/drodrolib/ui/graphics/BitmapHelper.java
+++ drodrolib/src/main/java/org/michenux/drodrolib/ui/graphics/BitmapHelper.java
@@ -1,11 +1,14 @@
 package org.michenux.drodrolib.ui.graphics;
 
+import android.annotation.TargetApi;
 import android.graphics.Bitmap;
+import android.os.Build;
 
 public class BitmapHelper {
 
+    @TargetApi(Build.VERSION_CODES.HONEYCOMB_MR1)
     public static int getSizeInBytes(Bitmap bitmap) {
-        if (android.os.Build.VERSION.SDK_INT>=12) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.HONEYCOMB_MR1) {
             return bitmap.getByteCount();
         } else {
             return bitmap.getRowBytes() * bitmap.getHeight();
